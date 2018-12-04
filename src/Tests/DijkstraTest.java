fc0a8928f89dff01c65db4d6a4753590022f2f56
Feffe <c.fredrik.aberg@gmail.com>
2018-12-04 11:11:05 +0100

    Add dijkstra solver test.

---
+++ src/Tests/DijkstraTest.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import mdsd.*;
+import org.junit.*;
+import simbad.sim.EnvironmentDescription;
+import simbad.sim.HorizontalWall;
+
+import java.awt.*;
+import java.util.List;
+
+
+public class DijkstraTest {
+
+    @Test
+    public void dijkstraCentralWallTest(){
+        EnvironmentDescription ed = new EnvironmentDescription();
+        GridManager gm = new GridManager();
+        gm.generateGrid(-5,-5,5,5,0.2);
+        gm.addHorizantalWall(new HorizontalWall(0.0f,5.0f, 4.4f, ed, Color.GRAY));
+        DijkstraSolver ds = new DijkstraSolver(gm);
+        List<GridElement> route = ds.solve(gm.translateToGrid(1.0d,0.0d),gm.translateToGrid(-1.0d,0.0d));
+        for (GridElement grd:route) {
+            System.out.println(String.valueOf(grd.getX())+", "+String.valueOf(grd.getZ()));
+        }
+        System.out.println(route.toString());
+        System.out.println(gm.gridToString(route));
+    }
+
+}
